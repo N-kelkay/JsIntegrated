005b24b3b4ab1deb3092bf071adde6defa4108dc
Natenael Kelkay <natikelkay@gmail.com>
2018-06-09 16:12:53 -0400

    made the addition log work

--- JsEvents/events.js
+++ JsEvents/events.js
@@ -2,12 +2,28 @@ var numOne = document.getElementById("num-one");
 var numTwo = document.getElementById("num-two");
 var addSum = document.getElementById("add-sum");
 
+
+/*
+Javascript events:
+- click
+- mouseenter
+- mouseleave
+- mousedown
+- mouseup
+- mousemove
+- keydown
+- keyup
+- blur
+- focus
+ */
 numOne.addEventListener("input", add);
-numTwo.addEventListener("input", add);
+numTwo.addEventListener("click", add);
 
 function add() {
-    var one = parseFloat(numOne.value) || 0;
-    var two = parseFloat(numTwo.value) || 0;
 
-    addSum.innerHTML = "your sum is: " + (one+two);
+    //ParseFloat changes it to a floating number
+    var x = parseFloat(numOne.value) || 0;
+    var y = parseFloat(numTwo.value) || 0;
+    // var sum = x+y;
+    addSum.innerHTML = "Your total is: " + (x + y);
 }
