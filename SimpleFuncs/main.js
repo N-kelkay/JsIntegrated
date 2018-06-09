5fbfc87b5a6e09d3db7e7550155dd86d277c0005
Natenael Kelkay <natikelkay@gmail.com>
2018-06-09 12:54:33 -0400

    loops

--- SimpleFuncs/main.js
+++ SimpleFuncs/main.js
@@ -1,8 +1,31 @@
-//Function: Piece of code that does one or more actions
 
-function add(a, b) {
-    return a + b;
+
+var myList = ["apples", "banana", "oranges"];
+
+//Goes through each index and runs the function for each value
+myList.forEach(function (value, index) {
+    console.log(value, index);
+});
+
+//shift takes out the first index and returns it
+myList.shift();
+
+//pop takes out the last index and returns it
+myList.pop();
+
+
+//Loops
+
+var x = 0;
+while (x<10) {
+    console.log("Tried it " + x);
+    x++;
 }
 
-var sum = add(1, 2);
-alert(sum);
+x = 0;
+//Do while does the process at least once, then checks if it is valid
+do {
+    console.log("Tried it " + x);
+    x++;
+}while (x < 10);
+
