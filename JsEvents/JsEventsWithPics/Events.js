07dce8e911c424b94af84512bc4a7b6e44044f4b
Natenael Kelkay <natikelkay@gmail.com>
2018-06-09 17:02:08 -0400

    finished faster way

--- JsEvents/JsEventsWithPics/Events.js
+++ JsEvents/JsEventsWithPics/Events.js
@@ -5,6 +5,7 @@ var brucePic = document.getElementById("bruce-pic");
 var ben = document.getElementById("ben");
 var benPic = document.getElementById("ben-pic");
 
+/*
 simon.addEventListener("click", function () {
     if(simonPic.className == "hide"){
         simonPic.className = "";
@@ -28,11 +29,25 @@ ben.addEventListener("click", function () {
         benPic.className = "hide";
     }
 });
+*/
 
-// simon.addEventListener("click", pickLink);
-// bruce.addEventListener("click", pickLink);
-// ben.addEventListener("click", pickLink);
-//
-// function pickLink() {
-//
-// }
+// above code is DRY code ^^^.
+// FASTER Way:
+
+simon.addEventListener("click", pickLink);
+bruce.addEventListener("click", pickLink);
+ben.addEventListener("click", pickLink);
+
+function pickLink() {
+    //gets the data-img class name
+    var thisId = this.attributes["data-img"].value;
+
+    //gets the whole element. Ex:
+    // <img class="hide" id="simon-pic" src="http://upload.wikimedia.org/wikipedia/commons/a/aa/Simon_Cowell_in_December_2011.jpg">
+    var pic = document.getElementById(thisId);
+    if(pic.className == "hide"){
+        pic.className = "";
+    }else{
+        pic.className = "hide";
+    }
+}
